ebb26948741ac0fbcc16e9c34b3150447d97c59d
Tanmoy Paul <pault@bxscience.edu>
2023-02-15 11:57:03 -0500

    Activity 3 files

---
+++ activity3.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MyConsoleApplication
+{
+    class Student
+    {
+        public string name;
+        public int id;
+    };
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var Serra = new Student();
+            Serra.name = "Serra";
+            Serra.id = 1;
+
+            var Chase = new Student();
+            Chase.name = "Chase";
+            Chase.id = 2;
+
+            var Tanmoy = new Student();
+            Tanmoy.name = "Tanmoy";
+            Tanmoy.id = 3;
+        }
+    }
+
+}
