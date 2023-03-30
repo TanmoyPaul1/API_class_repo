3ce0ee5a7f3d57b87ad2bc49d4270aed09efc5d2
Tanmoy Paul <pault@bxscience.edu>
2023-03-30 11:35:52 -0400

    Created customer/email api

---
+++ MyFirstAPI/MyFirstAPI/Models/Email.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace MyFirstAPI.Models
+{
+    public class Email
+    {
+        public int? EmailID { get; set; }
+        public string? EmailAddress { get; set; }
+        public bool? IsSubscribed { get; set; }
+    }
+}
